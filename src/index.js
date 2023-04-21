cf98a3d8c2e014096933ca36d6dcd7389b0c7b01
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-04-21 17:16:59 +0300

    update css

--- src/index.js
+++ src/index.js
@@ -1,147 +1,69 @@
-import './css/styles.css';
-
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImages } from './js/fetchImages';
+// import { renderGallery } from './js/renderGallery';
 
-import { createMarkup } from './js/createMarkup';
-import { PixabayAPI } from './js/PixabayAPI';
-import { refs } from './js/refs';
-import { notifyInit } from './js/notifyInit';
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
 
-const modalLightboxGallery = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-});
+let query = '';
+let page = 1;
+let simpleLightBox;
+const perPage = 40;
 
-refs.btnLoadMore.classList.add('is-hidden');
+searchForm.addEventListener('submit', onSearchForm);
 
-const pixaby = new PixabayAPI();
-
-const options = {
-  root: null,
-  rootMargin: '100px',
-  threshold: 1.0,
-};
-
-const loadMorePhotos = async function (entries, observer) {
-  entries.forEach(async entry => {
-    if (entry.isIntersecting) {
-      observer.unobserve(entry.target);
-      pixaby.incrementPage();
-
-      try {
-        const { hits } = await pixaby.getPhotos();
-        const markup = createMarkup(hits);
-        refs.gallery.insertAdjacentHTML('beforeend', markup);
-
-        if (pixaby.hasMorePhotos) {
-          const lastItem = document.querySelector('.gallery a:last-child');
-          observer.observe(lastItem);
-        } else
-          Notify.info(
-            "We're sorry, but you've reached the end of search results.",
-            notifyInit
-          );
-
-        modalLightboxGallery.refresh();
-        scrollPage();
-      } catch (error) {
-        Notify.failure(error.message, 'Something went wrong!', notifyInit);
-        clearPage();
-      } finally {
-      }
-    }
-  });
-};
-
-const observer = new IntersectionObserver(loadMorePhotos, options);
-
-const onSubmitClick = async event => {
-  event.preventDefault();
-
-  const {
-    elements: { searchQuery },
-  } = event.target;
-
-  const search_query = searchQuery.value.trim().toLowerCase();
-
-  if (!search_query) {
-    clearPage();
-    Notify.info('Enter data to search!', notifyInit);
-
-    refs.searchInput.placeholder = 'What`re we looking for?';
+function renderGallery(images) {
+  // Перевірка чи існує галерея перед вставкою даних
+  if (!gallery) {
     return;
   }
 
-  pixaby.query = search_query;
+  const markup = images
+    .map(image => {
+      const {
+        id,
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+      return `
+      <a href='${largeImageURL}' class="card-link js-card-link">
+      <div class="photo-card">
+        <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <div class="info-item info-item-likes">
+            <div class="box-likes"><b>Likes</b>
+            <span id="value">${likes}</span>
+            </div>
+          </div>
+          <p class="info-item">
+            <b>Views</b>
+            ${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            ${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            ${downloads}
+          </p>
+        </div>
+      </div>
+      </a>`;
+    })
+    .join('');
 
-  clearPage();
-
-  try {
-    const { hits, total } = await pixaby.getPhotos();
-
-    if (hits.length === 0) {
-      Notify.failure(
-        `Sorry, there are no images matching your ${search_query}. Please try again.`,
-        notifyInit
-      );
-
-      return;
-    }
-
-    const markup = createMarkup(hits);
-    refs.gallery.insertAdjacentHTML('beforeend', markup);
-
-    pixaby.setTotal(total);
-    Notify.success(`Hooray! We found ${total} images.`, notifyInit);
-
-    if (pixaby.hasMorePhotos) {
-      const lastItem = document.querySelector('.gallery a:last-child');
-      observer.observe(lastItem);
-    }
-
-    modalLightboxGallery.refresh();
-  } catch (error) {
-    Notify.failure(error.message, 'Something went wrong!', notifyInit);
-
-    clearPage();
-  } finally {
-  }
-};
-
-const onLoadMore = async () => {
-  pixaby.incrementPage();
-
-  if (!pixaby.hasMorePhotos) {
-    refs.btnLoadMore.classList.add('is-hidden');
-    Notify.info("We're sorry, but you've reached the end of search results.");
-    notifyInit;
-  }
-  try {
-    const { hits } = await pixaby.getPhotos();
-    const markup = createMarkup(hits);
-    refs.gallery.insertAdjacentHTML('beforeend', markup);
-
-    modalLightboxGallery.refresh();
-  } catch (error) {
-    Notify.failure(error.message, 'Something went wrong!', notifyInit);
-
-    clearPage();
-  }
-};
-
-function clearPage() {
-  pixaby.resetPage();
-  refs.gallery.innerHTML = '';
-  refs.btnLoadMore.classList.add('is-hidden');
-}
-
-refs.form.addEventListener('submit', onSubmitClick);
-refs.btnLoadMore.addEventListener('click', onLoadMore);
-
-function scrollPage() {
+  gallery.insertAdjacentHTML('beforeend', markup);
+  // Цей код дозволяє автоматично прокручувати сторінку на висоту 2 карток галереї, коли вона завантажується
   const { height: cardHeight } = document
-    .querySelector('.photo-gallery')
+    .querySelector('.gallery')
     .firstElementChild.getBoundingClientRect();
 
   window.scrollBy({
@@ -150,15 +72,81 @@ function scrollPage() {
   });
 }
 
-window.addEventListener('scroll', scrollFunction);
+function onSearchForm(e) {
+  e.preventDefault();
+  page = 1;
+  query = e.currentTarget.elements.searchQuery.value.trim();
+  gallery.innerHTML = '';
 
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    refs.btnUpWrapper.style.display = 'flex';
-  } else {
-    refs.btnUpWrapper.style.display = 'none';
+  if (query === '') {
+    Notiflix.Notify.failure(
+      'The search string cannot be empty. Please specify your search query.'
+    );
+    return;
+  }
+
+  fetchImages(query, page, perPage)
+    .then(data => {
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderGallery(data.hits);
+        simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      searchForm.reset();
+    });
+}
+
+function onloadMore() {
+  page += 1;
+  simpleLightBox.destroy();
+  // simpleLightBox.refresh();
+
+  fetchImages(query, page, perPage)
+    .then(data => {
+      renderGallery(data.hits);
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+      const totalPages = Math.ceil(data.totalHits / perPage);
+
+      if (page > totalPages) {
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function checkIfEndOfPage() {
+  return (
+    window.innerHeight + window.pageYOffset >=
+    document.documentElement.scrollHeight
+  );
+}
+
+// Функція, яка виконуеться, якщо користувач дійшов до кінця сторінки
+function showLoadMorePage() {
+  if (checkIfEndOfPage()) {
+    onloadMore();
   }
 }
-refs.btnUp.addEventListener('click', () => {
+
+// Додати подію на прокручування сторінки, яка викликає функцію showLoadMorePage
+window.addEventListener('scroll', showLoadMorePage);
+
+// кнопка “вгору”->
+arrowTop.onclick = function () {
   window.scrollTo({ top: 0, behavior: 'smooth' });
+  // після scrollTo відбудеться подія "scroll", тому стрілка автоматично сховається
+};
+
+window.addEventListener('scroll', function () {
+  arrowTop.hidden = scrollY < document.documentElement.clientHeight;
 });
